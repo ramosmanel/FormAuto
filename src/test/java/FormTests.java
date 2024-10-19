a7d1f8d51685377b19046f6793cd8459a5953e59
ramosmanel <emanuelramos20004@gmail.com>
2024-10-19 15:54:23 -0300

    fix: correction driver call

--- src/test/java/FormTests.java
+++ src/test/java/FormTests.java
@@ -1,22 +1,27 @@
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import pageobjects.FormPage;
 import setup.Driver;
 
+
 public class FormTests {
-    private Driver driver;
     FormPage formPage;
 
     @Before
     public void startDriver() {
-        driver.webDriver();
+        formPage = new FormPage(Driver.getDriver());
     }
 
     @After
     public void endDriver(){
-        driver.webDriver().close();
-        driver.webDriver().quit();
+        Driver.quitDriver();
     }
 
+    @Test
+    public void requieredField(){
+        formPage.patientField("");
+        formPage.requiredMessage(1);
+    }
 
 }
