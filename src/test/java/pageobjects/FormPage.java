a74aea11fe21d0e871f09e0359dd66990b57f589
ramosmanel <emanuelramos20004@gmail.com>
2024-10-19 15:55:44 -0300

    refactor: driver instance

--- src/test/java/pageobjects/FormPage.java
+++ src/test/java/pageobjects/FormPage.java
@@ -1,36 +1,30 @@
 package pageobjects;
 
-import org.junit.After;
-import org.junit.Before;
 import org.openqa.selenium.By;
-import setup.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
 
 public class FormPage {
-    private Driver driver;
+    private final WebDriver driver;
 
-    @Before
-    public void startDriver(){
-        driver.webDriver();
-    }
-
-    @After
-    public void endDriver(){
-        driver.webDriver().close();
-        driver.webDriver().quit();
+    public FormPage(WebDriver driver){
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
     }
 
     //Paciente
     public void patientField(String name) {
-        driver.webDriver().findElement(By.cssSelector("[aria-labelledby='i1']")).sendKeys(name);
+        driver.findElement(By.cssSelector("[aria-labelledby='i1']")).sendKeys(name);
     }
 
     public String requiredMessage(int i){
         String xpath = "//div[@class='o3Dpx']/div["+i+"]//div[@class='SL4Sz']";
-        return driver.webDriver().findElement(By.xpath(xpath)).getText();
+        return driver.findElement(By.xpath(xpath)).getText();
     }
 
     //Data
     public void DateField(String date) {
-        driver.webDriver().findElement(By.cssSelector("[type='date']")).sendKeys(date);
+        driver.findElement(By.cssSelector("[type='date']")).sendKeys(date);
     }
 }
