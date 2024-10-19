77e5c1110ca6109fbac2e4d71a7d9025792ff868
ramosmanel <emanuelramos20004@gmail.com>
2024-10-19 15:57:13 -0300

    refactor: correction driver call

--- src/test/java/steps/PatientSteps.java
+++ src/test/java/steps/PatientSteps.java
@@ -2,27 +2,13 @@ package steps;
 
 import io.cucumber.java.es.Dado;
 import io.cucumber.java.pt.Entao;
-import org.junit.After;
-import org.junit.Before;
 import pageobjects.FormPage;
-import setup.Driver;
+
 
 import static org.junit.Assert.assertEquals;
 
 public class PatientSteps {
-    Driver driver = new Driver();
-    FormPage formPage = new FormPage();
-
-    @Before
-    public void startDriver(){
-        driver.webDriver();
-    }
-
-    @After
-    public void endDriver(){
-        driver.webDriver().close();
-        driver.webDriver().quit();
-    }
+    FormPage formPage;
 
     @Dado("que nao preencha o campo paciente")
     public void que_nao_preencha_o_campo_paciente() {
