70cc97ffb1db6dd016595875fdc8b3c68210ab98
ramosmanel <emanuelramos20004@gmail.com>
2024-10-21 19:03:34 -0300

    feat: create utils directory

---
+++ src/test/java/utils/Messages.java
@@ -0,0 +1,6 @@
+package utils;
+
+public class Messages {
+    public static final String REQUIRED_FIELD_MESSAGE = " Esta pergunta é obrigatória";
+    public static final String INVALID_PATIENT_NAME_MESSAGE = " O nome do paciente informado não atende o padrão esperado.";
+}
