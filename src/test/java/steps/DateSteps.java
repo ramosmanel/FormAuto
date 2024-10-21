f2ffdff4c3a1a9330c0646dbf2059b0f1992ba9e
ramosmanel <emanuelramos20004@gmail.com>
2024-10-21 14:16:53 -0300

    feat: instantiating Form Page and preventing it from being null

--- src/test/java/steps/DateSteps.java
+++ src/test/java/steps/DateSteps.java
@@ -2,9 +2,10 @@ package steps;
 
 import io.cucumber.java.es.Dado;
 import pageobjects.FormPage;
+import setup.Driver;
 
 public class DateSteps {
-    FormPage formPage;
+    FormPage formPage = new FormPage(Driver.getDriver());
 
     @Dado("que nao preencha o campo data")
     public void que_nao_preencha_o_campo_data() {
