918f6378f96873c754fbc6d84dceefeb6203e56d
ramosmanel <emanuelramos20004@gmail.com>
2024-10-22 17:01:08 -0300

    feat: new tests for date field

--- src/test/java/FormTests.java
+++ src/test/java/FormTests.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import pageobjects.FormPage;
 import setup.Driver;
+import steps.DateSteps;
 import steps.PatientSteps;
 import utils.Messages;
 import utils.Screenshot;
@@ -12,6 +13,7 @@ import utils.Screenshot;
 public class FormTests {
     FormPage formPage;
     PatientSteps patientSteps = new PatientSteps();
+    DateSteps dateSteps = new DateSteps();
     Screenshot screenshot = new Screenshot();
 
     @Before
@@ -26,7 +28,7 @@ public class FormTests {
     }
 
     @Test
-    @DisplayName("Campo Obrigatório")
+    @DisplayName("CT01 - Campo Paciente Obrigatório")
     public void requieredPatientField(){
         patientSteps.que_eu_preencha_o_campo_paciente_com("");
         patientSteps.passe_para_o_campo_seguinte();
@@ -34,7 +36,7 @@ public class FormTests {
     }
 
     @Test
-    @DisplayName("Validando campo Paciente com valor válido")
+    @DisplayName("CT02 - Validando campo Paciente com valor válido")
     public void validPatient() {
         patientSteps.que_eu_preencha_o_campo_paciente_com("Nicholas Ferreira");
         patientSteps.passe_para_o_campo_seguinte();
@@ -42,7 +44,7 @@ public class FormTests {
     }
 
     @Test
-    @DisplayName("Campo do Paciente não pode conter mais de 5 caracteres repetidos")
+    @DisplayName("CT03 - Campo do Paciente não pode conter mais de 5 caracteres repetidos")
     public void characterRepetition(){
         patientSteps.que_eu_preencha_o_campo_paciente_com("AAAAAAAA");
         patientSteps.passe_para_o_campo_seguinte();
@@ -50,7 +52,7 @@ public class FormTests {
     }
 
     @Test
-    @DisplayName("Validando campo Paciente com valor inválido")
+    @DisplayName("CT04 - Validando campo Paciente com valor inválido")
     public void invalidPatient(){
         patientSteps.que_eu_preencha_o_campo_paciente_com("123 Ronaldin");
         patientSteps.passe_para_o_campo_seguinte();
@@ -58,7 +60,7 @@ public class FormTests {
     }
 
     @Test
-    @DisplayName("Máximo de caracteres atingidos")
+    @DisplayName("CT05 - Máximo de caracteres atingidos")
     public void characterLimit(){
         patientSteps.que_eu_preencha_o_campo_paciente_com("pneumoultramicroscopicossilicovulcanoconióticoanticonstitucionalissimamenteextraordinariamente");
         patientSteps.passe_para_o_campo_seguinte();
@@ -66,11 +68,74 @@ public class FormTests {
     }
 
     @Test
-    @DisplayName("Máximo de caracteres excedidos")
+    @DisplayName("CT06 - Máximo de caracteres excedidos")
     public void characterLimitExceeded() {
         patientSteps.que_eu_preencha_o_campo_paciente_com("pneumoultramicroscopicossilicovulcanoconióticoanticonstitucionalissimamenteextraordinariamente123");
         patientSteps.passe_para_o_campo_seguinte();
         patientSteps.o_sistema_deve_retornar_uma_mensagem_de(Messages.INVALID_PATIENT_NAME_MESSAGE, 1);
     }
 
+    @Test
+    @DisplayName("CT07 - Paciente sem sobrenome")
+    public void patientWithoutLastName(){
+        patientSteps.que_eu_preencha_o_campo_paciente_com("Jorge");
+        patientSteps.passe_para_o_campo_seguinte();
+        patientSteps.o_sistema_deve_retornar_uma_mensagem_de(Messages.INVALID_PATIENT_NAME_MESSAGE, 1);
+    }
+
+    @Test
+    @DisplayName("CT08 - Nome do paciente começando com espaço")
+    public void patientNameStartingWithSpace(){
+        patientSteps.que_eu_preencha_o_campo_paciente_com(" Jogue");
+        patientSteps.passe_para_o_campo_seguinte();
+        patientSteps.o_sistema_deve_retornar_uma_mensagem_de(Messages.INVALID_PATIENT_NAME_MESSAGE, 1);
+    }
+
+    @Test
+    @DisplayName("CT09 - Nome do paciente composto apenas por espaços")
+    public void compositePatientNameWithSpaceOnly() {
+        patientSteps.que_eu_preencha_o_campo_paciente_com("      ");
+        patientSteps.passe_para_o_campo_seguinte();
+        patientSteps.o_sistema_deve_retornar_uma_mensagem_de(Messages.INVALID_PATIENT_NAME_MESSAGE, 1);
+    }
+
+    @Test
+    @DisplayName("CT10 - Campo Data Obrigatorio")
+    public void requiredDateField(){
+        dateSteps.que_eu_preencha_o_campo_data_com("");
+        dateSteps.passe_para_o_campo_seguinte();
+        dateSteps.o_sistema_deve_retornar_uma_mensagem_de(Messages.REQUIRED_FIELD_MESSAGE, 2);
+    }
+
+    @Test
+    @DisplayName("CT11 - Campo Data com dados validos")
+    public void dateValid(){
+        dateSteps.que_eu_preencha_o_campo_data_com("24/04/2000");
+        dateSteps.passe_para_o_campo_seguinte();
+        dateSteps.o_sistema_deve_retornar_uma_mensagem_de("", 2);
+    }
+
+    @Test
+    @DisplayName("CT12 - Data invalida com valores no passado invalidos")
+    public void invalidDatePassed(){
+        dateSteps.que_eu_preencha_o_campo_data_com("01/01/1500");
+        dateSteps.passe_para_o_campo_seguinte();
+        dateSteps.o_sistema_deve_retornar_uma_mensagem_de(Messages.INVALID_DATE_VALUE_MESSAGE, 2);
+    }
+
+    @Test
+    @DisplayName("CT13 - Data invalida com valores futuros")
+    public void futureInvalidDate(){
+        dateSteps.que_eu_preencha_o_campo_data_com("01/01/2050");
+        dateSteps.passe_para_o_campo_seguinte();
+        dateSteps.o_sistema_deve_retornar_uma_mensagem_de(Messages.INVALID_DATE_VALUE_MESSAGE, 2);
+    }
+
+    @Test
+    @DisplayName("CT14 - Usuário menor de 18 anos")
+    public void underage(){
+        dateSteps.que_eu_preencha_o_campo_data_com("22/10/2021");
+        dateSteps.passe_para_o_campo_seguinte();
+        dateSteps.o_sistema_deve_retornar_uma_mensagem_de(Messages.INVALID_DATE_VALUE_MESSAGE, 2);
+    }
 }
