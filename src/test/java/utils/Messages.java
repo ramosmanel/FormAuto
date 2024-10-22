9341488a4d213b8a477761d2d6970440f9c1fe4a
ramosmanel <emanuelramos20004@gmail.com>
2024-10-22 16:58:17 -0300

    feat: add new messages

--- src/test/java/utils/Messages.java
+++ src/test/java/utils/Messages.java
@@ -3,4 +3,5 @@ package utils;
 public class Messages {
     public static final String REQUIRED_FIELD_MESSAGE = " Esta pergunta é obrigatória";
     public static final String INVALID_PATIENT_NAME_MESSAGE = " O nome do paciente informado não atende o padrão esperado.";
+    public static final String INVALID_DATE_VALUE_MESSAGE = " Data inválida";
 }
